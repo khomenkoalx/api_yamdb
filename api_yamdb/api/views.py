996c2591b1e26a3beadd4fd6f456b64199b3500a
Alex Khomenko <a.khomenko42@gmail.com>
2024-11-12 13:30:28 +0300

    Corrected part of PEP highlights

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from reviews.models import Category
 from .serializers import CategorySerializer
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
